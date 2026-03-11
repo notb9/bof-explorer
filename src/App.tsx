39ac882849574f20a6fa3630ed978e9e59ba7d97
notb9 <12910494+notb9@users.noreply.github.com>
2026-03-11 21:24:58 +0100

    Add contact details

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { SearchForm } from "./components/SearchForm";
 import { ParameterPane } from "./components/ParameterPane";
 import { useBofs } from "./hooks/useBofs";
 import { useState } from "react";
-import { Container, Row, Tab, Tabs, Accordion } from "react-bootstrap";
+import { Container, Row, Col, Tab, Tabs, Accordion } from "react-bootstrap";
 import { CodeBlock } from "./components/CodeBlock";
 import { Banner } from "./components/Banner";
 import { AnalysisPane } from "./components/AnalysisPane";
@@ -36,39 +36,48 @@ function App() {
           <SearchForm allBofs={allBofs} onSelect={callback} />
         </Row>
         {bof && (
-          <Row className="px-2">
-            <Accordion defaultActiveKey="0" className="px-0 mb-4">
-              <Accordion.Item eventKey="0">
-                <Accordion.Header>{bof.name} usage</Accordion.Header>
-                <Accordion.Body>
-                  <pre className="bof-usage">{bof.description}</pre>
-                  <a
-                    target={"_blank"}
-                    href={bof.url}
-                    className="btn btn-outline-light"
-                  >
-                    {"View on GitHub"} <i className="bi bi-github mx-2"></i>
-                  </a>
-                </Accordion.Body>
-              </Accordion.Item>
-            </Accordion>
-            <Tabs defaultActiveKey="parameters">
-              <Tab eventKey="parameters" title="Arguments" className="py-3">
-                <ParameterPane key={bof.name} bof={bof} />
-              </Tab>
-              <Tab eventKey="cna" title="CNA" className="py-3">
-                <h2> CNA </h2>
-                <CodeBlock code={bof.cna} language="groovy" />
-              </Tab>
-              <Tab eventKey="code" title="Entrypoint" className="py-3">
-                <p>The entrypoint of the Beacon Object File.</p>
-                <CodeBlock code={bof.code} language="c" />
-              </Tab>
-              <Tab eventKey="analysis" title="Analysis" className="py-3">
-                <AnalysisPane bof={bof} />
-              </Tab>
-            </Tabs>
-          </Row>
+          <>
+            <Row className="px-2">
+              <Accordion defaultActiveKey="0" className="px-0 mb-4">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>{bof.name} usage</Accordion.Header>
+                  <Accordion.Body>
+                    <pre className="bof-usage">{bof.description}</pre>
+                    <a
+                      target={"_blank"}
+                      href={bof.url}
+                      className="btn btn-outline-light"
+                    >
+                      {"View on GitHub"} <i className="bi bi-github mx-2"></i>
+                    </a>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+              <Tabs defaultActiveKey="parameters">
+                <Tab eventKey="parameters" title="Arguments" className="py-3">
+                  <ParameterPane key={bof.name} bof={bof} />
+                </Tab>
+                <Tab eventKey="cna" title="CNA" className="py-3">
+                  <h2> CNA </h2>
+                  <CodeBlock code={bof.cna} language="groovy" />
+                </Tab>
+                <Tab eventKey="code" title="Entrypoint" className="py-3">
+                  <p>The entrypoint of the Beacon Object File.</p>
+                  <CodeBlock code={bof.code} language="c" />
+                </Tab>
+                <Tab eventKey="analysis" title="Analysis" className="py-3">
+                  <AnalysisPane bof={bof} />
+                </Tab>
+              </Tabs>
+            </Row>
+            <Row className="pt-4">
+              <Col className="text-center">
+                <small className="text-muted fst-italic footer">
+                  <a href="https://github.com/notb9/bof-explorer/pulls">{"- Report a bug -"}</a>
+                </small>
+              </Col>
+            </Row>
+          </>
         )}
       </Container>
     </Container>
